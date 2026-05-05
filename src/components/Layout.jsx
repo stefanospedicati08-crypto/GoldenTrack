45dbaea36eadd13904252700d1dc6119e46312d4
Stefano Spedicati <stefanospedicati08@gmail.com>
2026-05-05 21:46:11 +0000

    Visual edits

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -48,7 +48,7 @@ export default function Layout() {
           </div>
           <div>
             <h1 className="font-heading font-bold text-lg leading-tight">Golden Track</h1>
-            <p className="text-xs text-muted-foreground">be</p>
+            <p className="text-xs text-muted-foreground">believe </p>
           </div>
         </div>
 
